089f32355511a657bd5fe07d50a76161f491a8c3
Theodor Moroianu <theodor.moroianu@gmail.com>
2023-04-07 17:54:46 +0300

    Update stats.py

--- stats.py
+++ stats.py
@@ -15,12 +15,14 @@ games_by_average_elo_buckets = defaultdict(lambda: 0)
 DISCARD_THRESHOLD = 100
 nr_discarded_games = 0
 
+nr_played_games = 0
+
 openings_frequency = defaultdict(lambda: 0)
 # (#white win, #black win, #draw)
 openings_outcomes = defaultdict(lambda: (0, 0, 0))
 
 #%%
-for _ in tqdm(range(TOTAL_NR_GAMES)):
+for _ in tqdm(range(150000)):
     game = pgn.read_game(db)
     h = game.headers
     ELO_W, ELO_B = int(h["WhiteElo"]), int(h["BlackElo"])
@@ -33,6 +35,8 @@ for _ in tqdm(range(TOTAL_NR_GAMES)):
     if result == "*":
         # game is outgoing, silent ignore
         continue
+
+    nr_played_games += 1
 
     timecontrol[h["TimeControl"]] += 1
     openings_frequency[h["Opening"]] += 1
@@ -55,5 +59,49 @@ for _ in tqdm(range(TOTAL_NR_GAMES)):
     )
 
     games_by_average_elo_buckets[(ELO_W + ELO_B) // (2 * BUCKET_SIZE)] += 1
+
+# %%
+
+THRESHOLD_BULLET = 180
+THRESHOLD_BLITZ = 5*60
+THRESHOLD_RAPID = 30*60
+
+# PRINT STATISTICS
+
+#%%
+
+a = sorted([(i, games_by_average_elo_buckets[i]) for i in games_by_average_elo_buckets])
+for i in a:
+    print(f"| {i[0] * 100} - {i[0] * 100 + 99} | {(i[1] / nr_played_games * 100):.4f}% |")
+
+
+#%%
+a = sorted([(timecontrol[i], i) for i in timecontrol])[::-1][:10]
+for i in a:
+    print(f"| {i[0] / nr_played_games * 100:.4f}% | {i[1]} |")
+
+# %%
+#%%
+a = sorted([(openings_frequency[i], i) for i in openings_frequency])[::-1][:15]
+for i in a:
+    print(f"| {i[0] / nr_played_games * 100:.4f}% | {i[1]} |")
+
+# %%
+
+a = sorted(
+    [(openings_outcomes[i][0] / sum(openings_outcomes[i]) * 100, openings_outcomes[i], i)
+     for i in openings_outcomes if sum(openings_outcomes[i]) > 20]
+)[::-1][:15]
+for i in a:
+    print(f"| {i[2]} | {i[0]:.4f}% |")
+
+# %%
+
+a = sorted(
+    [(openings_outcomes[i][1] / sum(openings_outcomes[i]) * 100, openings_outcomes[i], i)
+     for i in openings_outcomes if sum(openings_outcomes[i]) > 20]
+)[::-1][:15]
+for i in a:
+    print(f"| {i[2]} | {i[0]:.4f}% |")
 
 # %%
