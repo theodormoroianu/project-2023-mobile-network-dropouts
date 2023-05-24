08b2601ea1c78d41115cb30325405154eb793e2c
Theodor Moroianu <theodor.moroianu@gmail.com>
2023-05-24 21:23:39 +0200

    add stuff

--- frontend/src/components/root.tsx
+++ frontend/src/components/root.tsx
@@ -1,47 +1,55 @@
 import { Alignment, Button, Classes, Navbar, NavbarDivider, NavbarGroup, NavbarHeading } from '@blueprintjs/core';
 import { useState } from 'react';
-import { HeatMapViz } from '../d3_visualizations/heatmap_viz';
 import { EloGamesDistributionViz } from '../d3_visualizations/elo_games_distribution_viz';
 import { D3VizAndChessboardSplitScreen } from './viz_and_chess_wrapper';
 import { AverageGameLengthHistViz } from '../d3_visualizations/average_game_length_hist_viz';
 import SideBySideVizWrapper from './side_by_side_viz_wrapper';
 import { NrMovesPerGamePerEloBucket } from '../d3_visualizations/nr_moves_per_game_per_elo_bucket';
 import { EloBucketsViewer } from './elo_buckets_viewer';
+import { Home } from './home';
 
 function Root() {
     // stores the view we want to display
-    let [view, setView] = useState("elo_games_distribution");
+    let [view, setView] = useState("home");
 
     return (
-        <div style={{ "width": "100%" }}>
-            <Navbar>
+        <div style={{ "width": "100%", "height": "100%" }}>
+            <Navbar fixedToTop>
                 <NavbarGroup align={Alignment.LEFT}>
                     <NavbarHeading>Chess Visualizer</NavbarHeading>
                     <NavbarDivider />
                     <Button
                         className={Classes.MINIMAL}
                         icon="home"
+                        text="home"
+                        onClick={() => setView("home")}
+                        active={view === "home"}
+                    />
+                    <Button
+                        className={Classes.MINIMAL}
+                        icon="model"
                         text="ELO"
                         onClick={() => setView("elo_games_distribution")}
                         active={view === "elo_games_distribution"}
                     />
                     <Button
                         className={Classes.MINIMAL}
-                        icon="model" text="Average Length"
+                        icon="comparison" text="Average Length"
                         onClick={() => setView("average_game_length_his_viz")}
                         active={view === "average_game_length_his_viz"}
                     />
                     <Button
                         className={Classes.MINIMAL}
-                        icon="comparison"
+                        icon="filter"
                         text="ELO Specifics"
                         onClick={() => setView("elo-specifics")}
                         active={view === "elo-specifics"}
                     />
                 </NavbarGroup>
             </Navbar>
-            <div style={{ "width": "100%", "padding": "30px" }}>
+            <div style={{ "width": "100%", "height": "100%", "padding": "30px", "paddingTop": "80px" }}>
                 {/* We display the view we are interested in. */}
+                {view === "home" && <Home />}
                 {view === "elo_games_distribution" && <D3VizAndChessboardSplitScreen d3Viz={EloGamesDistributionViz} />}
                 {view === "average_game_length_his_viz" &&
                     <SideBySideVizWrapper
---
+++ misc/generate_chess_average_per_country.py
@@ -0,0 +1,38 @@
+import os
+from collections import defaultdict
+
+try:
+    data = open("standard_rating_list.txt", "r").readlines()[1:]
+except:
+    os.system("wget http://ratings.fide.com/download/standard_rating_list.zip")
+    os.system("unzip standard_rating_list.zip")
+    data = open("standard_rating_list.txt", "r").readlines()[1:]
+
+
+def parse_line(line):
+    # returns name, country code, rating
+    return line[15:76].strip(), line[76:79], int(line[113:117])
+
+countries = defaultdict(lambda: [])
+
+for player, country, rating in [parse_line(i) for i in data]:
+    countries[country].append((player, rating))
+
+OUTPUT_FILE = "country_code_to_rating.csv"
+data = []
+
+for country in countries:
+    max_rating = max(countries[country])
+    data.append((country, max_rating[1], max_rating[0].replace(',', '')))
+
+data.sort(key=lambda x: x[1], reverse=True)
+
+with open(OUTPUT_FILE, "w") as fout:
+    fout.write("code,best_rating,best_player\n")
+    for country, rating, name in data:
+        fout.write(f"{country},{rating},{name}\n")
+
+PUBLIC_PATH = f"{os.path.dirname(os.path.dirname(__file__))}/frontend/public/map/{OUTPUT_FILE}"
+store = input(f"Store to {PUBLIC_PATH} (Y/N)? ")
+if store[0] == 'Y' or store[0] == 'y':
+    os.system(f"cp {OUTPUT_FILE} {PUBLIC_PATH}")
