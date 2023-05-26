eaec524ac3ac930bfdb82766dc061d489654ec44
tifui-alexandru <itifui00@gmail.com>
2023-05-26 15:43:14 +0200

    new checkpoint

--- frontend/src/components/piece_placement_heatmap.tsx
+++ frontend/src/components/piece_placement_heatmap.tsx
@@ -1,12 +1,9 @@
-import { Card, Elevation, NonIdealState, Tab, Tabs, Checkbox } from '@blueprintjs/core';
-import { useEffect, useState } from 'react';
-import { EloBucketList, EloBucketStats, FetchEloBucketStats } from '../api/elo_bucket_stats_api';
-import { FetchEloBucketList } from '../api/elo_bucket_stats_api';
-import { PieCharViewer } from './pie_chart_viewer';
+import { Card, Elevation, RadioGroup, Radio, Slider } from '@blueprintjs/core';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { EloBucketStats } from '../api/elo_bucket_stats_api';
 import ReactApexChart from 'react-apexcharts';
 import { ApexOptions } from 'apexcharts';
 import { rgb } from 'd3';
-import { ChessBoardFenExplorer } from './chessboard';
 
 interface HeatmapStats {
     eloBucketStats: EloBucketStats | null,
@@ -102,6 +99,65 @@ const Heatmap = ({ eloBucketStats, pieceType, moveNo, playerColor }: HeatmapStat
 
 }
 
+interface PiecesRadioProps {
+  pieceType: string,
+  setPieceType: Dispatch<SetStateAction<string>>
+}
+const PiecesRadio = ({ pieceType, setPieceType } : PiecesRadioProps) => {
+  return (
+     <div style={{
+             width: 600,
+             paddingLeft: 30
+        }}>
+         <div style={{
+                 paddingTop: 0,
+                 paddingLeft: 190
+        }}>
+            <h4>Select Piece Type</h4>
+        </div>
+        <RadioGroup
+            onChange={(event) => (setPieceType(event.currentTarget.value))}
+            selectedValue={pieceType}
+            inline={true}
+        >
+            <Radio label="Pawn" value="pawn" />
+            <Radio label="Knight" value="knight" />
+            <Radio label="Bishop" value="bishop" />
+            <Radio label="Rook" value="rook" />
+            <Radio label="Queen" value="queen" />
+            <Radio label="King" value="king" />
+        </RadioGroup>
+    </div>
+  )
+}
+
+interface MoveNoSliderProps {
+  moveNo: number,
+  setMoveNo: Dispatch<SetStateAction<number>>
+}
+const MoveNoSlider = ({ moveNo, setMoveNo } : MoveNoSliderProps) => {
+  return (
+     <div style={{
+            width: 600
+        }}>
+         <div style={{
+                 paddingTop: 0,
+                 paddingLeft: 230
+        }}>
+            <h4>Select Move Number</h4>
+        </div>
+            <Slider
+                min={0}
+                max={100}
+                stepSize={1}
+                labelStepSize={10}
+                value={moveNo}
+                onChange={(newMoveNo) => (setMoveNo(newMoveNo))}
+            />
+     </div >
+  )
+}
+
 interface PiecesPositionsHeatmapStats {
     eloBucketStats: EloBucketStats | null
 }
@@ -109,15 +165,32 @@ const PiecesPositionsHeatmap = ({ eloBucketStats }: PiecesPositionsHeatmapStats)
     const [pieceType, setPieceType] = useState("pawn");
     const [moveNo, setMoveNo] = useState(0);
 
-   return <div style={{
-        "display": "flex",
-        "flexDirection": "row",
-        "width": "100%",
-        "height": "100%"
-   }}>
-        <Heatmap eloBucketStats={eloBucketStats} pieceType={pieceType} moveNo={moveNo} playerColor={"white"}/>
-        <Heatmap eloBucketStats={eloBucketStats} pieceType={pieceType} moveNo={moveNo} playerColor={"black"}/>
-   </div>
+    return <>
+      <div style={{
+          "display": "flex",
+          "flexDirection": "row",
+          "justifyContent": "space-between",
+          "width": "88%",
+          "margin": "2%",
+      }}>
+          <Card interactive={true} elevation={Elevation.TWO}>
+              <PiecesRadio pieceType={pieceType} setPieceType={setPieceType}/>
+          </Card>
+          <Card interactive={true} elevation={Elevation.TWO}>
+              <MoveNoSlider moveNo={moveNo} setMoveNo={setMoveNo}/>
+          </Card>
+      </div>
+
+      <div style={{
+          "display": "flex",
+          "flexDirection": "row",
+          "width": "100%",
+          "height": "100%"
+      }}>
+          <Heatmap eloBucketStats={eloBucketStats} pieceType={pieceType} moveNo={moveNo} playerColor={"white"}/>
+          <Heatmap eloBucketStats={eloBucketStats} pieceType={pieceType} moveNo={moveNo} playerColor={"black"}/>
+      </div>
+    </>
 }
 
 export default PiecesPositionsHeatmap;
